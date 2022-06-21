303c06adc8c718d6338b91bb1ad40ebe8d1e0c94
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-20 20:09:10 -0400

    prizes and winners pages

---
+++ src/routes/prizes.svelte
@@ -0,0 +1,10 @@
+<h1>🥇</h1>
+<h1>🥈</h1>
+<h1>🥉</h1>
+
+<style>
+	h1 {
+		font-size: 15rem;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/winners.svelte
@@ -0,0 +1,9 @@
+<img src="https://i.giphy.com/media/BeXtp0K1wWcSdevewG/giphy.webp" alt="winner" />
+
+<style>
+	img {
+		width: 500px;
+		height: 500px;
+		margin: 5rem auto;
+	}
+</style>
