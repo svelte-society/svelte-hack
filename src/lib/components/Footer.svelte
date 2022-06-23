dd1f29648dc9081445962efc42904ced9936f66c
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-22 22:21:03 -0400

    footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+	Made with 🧡 by <a href="https://sveltesociety.dev/" target="_blank">Svelte Society</a>
+</footer>
+<div class="br-md" />
+
+<style>
+	footer {
+		color: var(--dark-a);
+		text-align: center;
+		font-size: var(--font-md);
+		font-family: var(--font-mono);
+	}
+</style>
