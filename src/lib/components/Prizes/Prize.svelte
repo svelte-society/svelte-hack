5895ceab2b2f6467d6061358b5ea61ac6bd54e8b
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-22 22:19:57 -0400

    prizes components

---
+++ src/lib/components/Prizes/Prize.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	export let prize: {
+		name: string
+		description: string
+		award: string
+		icon: string
+	}
+
+	const { name, description, award, icon } = prize
+</script>
+
+<div class="container">
+	<div class="row">
+		<div class="card">
+			<div class="row">
+				<div class="icon">{icon}</div>
+				<h2>{name}</h2>
+				<div class="award">{award}</div>
+			</div>
+			<p>{@html description}</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
+
+	.card {
+		box-shadow: var(--shadow-sm);
+	}
+
+	.icon {
+		font-size: 40px;
+		margin-right: 1rem;
+	}
+
+	.award {
+		margin-left: auto;
+		font-size: var(--font-lg);
+	}
+
+	:global(pre) {
+		display: inline;
+		padding: 4px;
+
+		color: var(--brand-a);
+		background: var(--light-b);
+
+		font-weight: 700;
+	}
+</style>
---
+++ src/lib/components/Prizes/Prizes.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Prize from './Prize.svelte'
+
+	const prizes = [
+		{
+			name: 'Best Application',
+			description: `Applications will be judged based on their quality, accessibility, and utility.  May the best app win!`,
+			award: '$4,000',
+			icon: '🏆'
+		},
+		{
+			name: 'Best Utility',
+			description: `With the addition of <a href="https://kit.svelte.dev/docs/packaging" target="_blank"><pre>svelte-kit package</pre></a>, it's now easier than ever to publish NPM packages with svelte.  This category is for packages that support and enrich the fast-growing ecosystem of tools in Svelte community.  Examples would be a UI or component Library, a useful wrapper for a third-party library, or a tool for use in Svelte projects.`,
+			award: '$4,000',
+			icon: '🧰'
+		},
+		{
+			name: 'Best Newcomer Project',
+			description: `This category is for developers that are new to the Svelte ecosystem.  There are no restrictions to the type of project that can be submitted.  If you've been considering trying Svelte but haven't made the plunge, this is the category for you!`,
+			award: '$4,000',
+			icon: '🪺'
+		}
+	]
+</script>
+
+<section class="col">
+	{#each prizes as prize}
+		<Prize {prize} />
+	{/each}
+</section>
+
+<style>
+	.col {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	}
+</style>
