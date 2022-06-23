7a3db503142077bfde2d09ef26cbad453cda3ab5
Fractal <55504972+FractalHQ@users.noreply.github.com>
2022-06-22 22:19:18 -0400

    prizes page

--- src/routes/prizes.svelte
+++ src/routes/prizes.svelte
@@ -1,10 +1,44 @@
-<h1>🥇</h1>
-<h1>🥈</h1>
-<h1>🥉</h1>
+<script>
+	import BlobA from '$lib/graphics/BlobA.svelte'
+	import { Prizes } from '$lib/components'
+</script>
+
+<section>
+	<div class="br-sm" />
+
+	<h1>Prizes</h1>
+
+	<div class="br-md" />
+
+	<Prizes />
+
+	<div class="blob-a">
+		<BlobA />
+	</div>
+</section>
 
 <style>
 	h1 {
-		font-size: 15rem;
+		font-size: var(--font-xxl);
 		text-align: center;
+	}
+
+	.blob-a {
+		position: absolute;
+		bottom: -30%;
+		left: 0;
+		z-index: -1;
+	}
+
+	section {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+
+		margin: 0;
+		padding-bottom: 5rem;
+
+		overflow: hidden;
 	}
 </style>
