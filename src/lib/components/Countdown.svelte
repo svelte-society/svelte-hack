fc787253e455d1b654149656275c6fd136458eda
Fractal <55504972+FractalHQ@users.noreply.github.com>
2023-02-09 17:37:05 -0500

    [fix] countdown date / style

--- src/lib/components/Countdown.svelte
+++ src/lib/components/Countdown.svelte
@@ -2,36 +2,52 @@
 	import { intervalToDuration, formatDuration } from 'date-fns'
 	import { onMount } from 'svelte'
 
-	const HACKATHON_DATE = 1659326400000
+	const HACKATHON_DATE = new Date(2023, 3, 24, 10, 11, 12)
 
 	let now = Date.now()
 
-	$: duration = intervalToDuration({
-		start: HACKATHON_DATE,
-		end: now
-	})
+	$: duration = formatDuration(
+		intervalToDuration({
+			start: HACKATHON_DATE,
+			end: now
+		}),
+		{ zero: true, format: ['months', 'weeks', 'days', 'hours', 'minutes'] }
+	)
 
 	onMount(() => {
-		const interval = setInterval(() => (now = Date.now()), 500)
+		const interval = setInterval(() => {
+			now = Date.now()
+		}, 5000)
 		return () => clearInterval(interval)
 	})
 </script>
 
 <div class="clock">
 	<p class="clockDisplay colorful">
-		{formatDuration(duration, { zero: true, format: ['months', 'weeks', 'days', 'hours', 'minutes'] })}
+		{#key now}
+			{#each duration.split(' ') ?? [duration] as _, i}
+				<span class:word={i % 2 === 1}>{_ + ' '}</span>
+			{/each}
+		{/key}
 	</p>
 </div>
 
 <style>
 	.clock {
 		margin: 0 auto;
+		width: fit-content;
 	}
 
 	p {
 		color: var(--fg-b);
 
-		font-size: clamp(1.25rem, 3rem, 4vw);
-		font-family: var(--font-mono);
+		font-size: clamp(1.25rem, 2rem, 4vw);
+		font-family: var(--font-b);
+
+		letter-spacing: 0.25rem;
+	}
+
+	.word {
+		font-size: var(--font-md);
 	}
 </style>
