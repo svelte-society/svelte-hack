af3d08e31aaf108c6d4106071b9c5a16193dd0fb
Fractal <55504972+FractalHQ@users.noreply.github.com>
2023-02-13 13:52:43 -0500

    [feat] confetti

---
+++ src/routes/submit/Confetti.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { confetti } from '@neoconfetti/svelte';
+</script>
+
+{#await new Promise((resolve) => setTimeout(resolve, 200)) then _}
+	<div
+		class="success"
+		use:confetti={{
+			colors: ['var(--brand-a)', 'var(--bg-c)', 'var(--fg-c)'],
+			destroyAfterDone: true,
+			duration: 5000,
+			stageHeight: window?.innerHeight,
+			particleSize: 5,
+			particleShape: 'mix',
+			particleCount: 200,
+			force: 0.5
+		}}
+	/>
+{/await}
