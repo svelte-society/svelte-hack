78d9ca00a7a47276ca8c493e1944066ed9980ab1
Fractal <55504972+FractalHQ@users.noreply.github.com>
2023-02-13 13:52:30 -0500

    [fix] allow empty strings in optional fields
    Co-authored-by: Willow (GHOST) <ghostdevv@users.noreply.github.com>

--- src/routes/submit/+page.server.ts
+++ src/routes/submit/+page.server.ts
@@ -1,8 +1,8 @@
-import type { Actions } from './$types'
+import type { Actions } from './$types';
 
-import { SUBMISSIONS_KEY } from '$env/static/private'
-import { fail } from '@sveltejs/kit'
-import { z } from 'zod'
+import { SUBMISSIONS_KEY } from '$env/static/private';
+import { fail } from '@sveltejs/kit';
+import { z } from 'zod';
 
 const schema = z.object({
 	authorOne: z.string({ required_error: 'Author is required' }).trim().min(1, "Can't be empty"),
@@ -10,29 +10,47 @@ const schema = z.object({
 	authorThree: z.string({ required_error: 'Author is required' }).trim().optional(),
 	github: z.string({ required_error: 'GitHub is required' }).url(),
 	demo: z.string({ required_error: 'Demo is required' }).url(),
-	title: z.string({ required_error: 'Title is required' }).trim().min(1, "Can't be empty").max(64, 'Max length is 64 chars'),
-	twitter: z.string({ required_error: 'Twitter is required' }).url().optional(),
-	description: z.string({ required_error: 'Description is required' }).trim().min(1, "Can't be empty").max(256, 'Max length is 256 chars')
-})
+	title: z
+		.string({ required_error: 'Title is required' })
+		.trim()
+		.min(1, "Can't be empty")
+		.max(64, 'Max length is 64 chars'),
+	twitter: z
+		.string({ invalid_type_error: 'Invalid URL' })
+		.trim()
+		.url()
+		.optional()
+		.or(z.literal('')),
+	description: z
+		.string({ required_error: 'Description is required' })
+		.trim()
+		.min(1, "Can't be empty")
+		.max(256, 'Max length is 256 chars')
+});
 
-const url = `https://hack-api.sveltesociety.dev/api/collections/submissions/records?key=${SUBMISSIONS_KEY}`
+const url = `https://hack-api.sveltesociety.dev/api/collections/submissions/records?key=${SUBMISSIONS_KEY}`;
 
 // TODO Rate Limit of some kind?
 
 export const actions: Actions = {
 	async default({ request, fetch }) {
-		const raw_data: Partial<z.infer<typeof schema>> = Object.fromEntries(await request.formData())
-		const result = await schema.safeParseAsync(raw_data)
+		const raw_data: Partial<z.infer<typeof schema>> = Object.fromEntries(
+			await request.formData()
+		);
+
+		console.log({ raw_data });
+
+		const result = await schema.safeParseAsync(raw_data);
 
 		// If the submission is invalid return the errors to the frontend
 		if (!result.success) {
-			const errors = result.error.flatten()
+			const errors = result.error.flatten();
 
 			return fail(400, {
 				fieldErrors: errors.fieldErrors,
 				success: false,
 				fields: raw_data
-			})
+			});
 		}
 
 		try {
@@ -43,21 +61,21 @@ export const actions: Actions = {
 				headers: {
 					'content-type': 'application/json'
 				}
-			})
+			});
 
 			if (!response.ok || response.status !== 200) {
-				throw new Error(`${response.status} Failed to submit`)
+				throw new Error(`${response.status} Failed to submit`);
 			}
 		} catch (e) {
 			return fail(400, {
 				error: (e as any)?.message || 'Failed to submit',
 				success: false,
 				fields: raw_data
-			})
+			});
 		}
 
 		return {
 			success: true
-		}
+		};
 	}
-}
+};
