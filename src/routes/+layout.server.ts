80715116d5544983fdeb4bfc2881d4bcb54f5540
GHOST <ghostdevbusiness@gmail.com>
2023-05-08 23:28:56 +0000

    chore: completely remove pocketbase stuff

--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
-	return {
-		loggedIn: !!locals.user,
-		user: locals.user ? { email: locals.user.email } : undefined
-	};
-};
