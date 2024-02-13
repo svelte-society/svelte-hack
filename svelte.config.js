d89bcf39aad440c8c6a9c955d327b1d60491dd95
GHOST <ghostdevbusiness@gmail.com>
2024-02-13 03:46:29 +0000

    refactor: switch to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import vercel from '@sveltejs/adapter-vercel'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
+import auto from '@sveltejs/adapter-auto'
 import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [preprocess(), mdsvex(mdsvexConfig)],
+
 	kit: {
-		adapter: vercel(),
+		adapter: auto(),
 		csrf: {
 			checkOrigin: process.env['NODE_ENV'] != 'development',
 		},
 	},
+
 	vitePlugin: {
 		// https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md#inspector
 		inspector: {
