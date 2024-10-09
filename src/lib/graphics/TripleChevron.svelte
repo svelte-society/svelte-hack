805e8ee93e445d47ed1f4886c727843f941ccd0f
Braden Wiggins <braden@fractal-hq.com>
2024-10-09 08:29:35 -0400

    feat: animated chevron

--- src/lib/graphics/TripleChevron.svelte
+++ src/lib/graphics/TripleChevron.svelte
@@ -7,22 +7,25 @@
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<path
+			class="a"
 			d="M1 13.3047L7 7.30469L1 1.30469"
-			stroke="#FF3E00"
+			stroke="currentColor"
 			stroke-width="1.5"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 		/>
 		<path
+			class="b"
 			d="M8 13.3047L14 7.30469L8 1.30469"
-			stroke="#FF3E00"
+			stroke="currentColor"
 			stroke-width="1.5"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 		/>
 		<path
+			class="c"
 			d="M15 13.3047L21 7.30469L15 1.30469"
-			stroke="#FF3E00"
+			stroke="currentColor"
 			stroke-width="1.5"
 			stroke-linecap="round"
 			stroke-linejoin="round"
@@ -30,9 +33,47 @@
 	</svg>
 </div>
 
-<style>
+<style lang="scss">
 	.triple-chevron {
+		--color: #ff3e00;
+		color: var(--color);
 		width: var(--size, 20px);
-		transform: translateY(3px);
+		transform: translate(12px, 1px);
+	}
+
+	svg {
+		overflow: visible;
+	}
+
+	path {
+		animation-name: animate;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+		animation-play-state: paused;
+	}
+
+	.a {
+		animation-delay: 0s;
+	}
+	.b {
+		animation-delay: 0.05s;
+	}
+	.c {
+		animation-delay: 0.1s;
+	}
+
+	@keyframes animate {
+		0% {
+			color: var(--color);
+			transform: translateX(0);
+		}
+		10% {
+			color: #ff3e00;
+			transform: translateX(4px);
+		}
+		50% {
+			color: var(--color);
+			transform: translateX(0);
+		}
 	}
 </style>
