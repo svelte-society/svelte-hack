a63b5710bf553df80cbbd05e9476303597f657d0
Braden Wiggins <braden@fractal-hq.com>
2024-10-10 08:32:19 -0400

    feat: new goos

--- src/routes/2024/+page.svelte
+++ src/routes/2024/+page.svelte
@@ -5,43 +5,46 @@
 	import Hero from '$lib/years/2024/Hero.svelte'
 </script>
 
-<div class="br-md" />
+<div class="br-md"></div>
 
 <section class="hero row">
 	<Hero />
 	<Scene />
 </section>
 
-<div class="br-xl" />
+<div class="br-xl"></div>
 
 <!-- <Countdown /> -->
 
 <!-- <Date /> -->
 
-<!-- <div class="br-xl" /> -->
-
-<span id="about" />
+<span id="about"></span>
 <About />
 
-<div class="br-lg" />
+<div class="br-lg"></div>
 
 <Sponsors />
 
-<div class="br-lg" />
-<div class="br-lg" />
+<div class="br-lg"></div>
+<div class="br-lg"></div>
 
 <style>
 	section.hero {
 		display: unset;
+		position: relative;
 	}
 
-	@media screen and (min-width: 1000px) {
+	@media screen and (width >= 1000px) {
 		section.hero {
 			display: flex;
 			align-items: center;
 
 			width: 100%;
 			max-width: min(95vw, var(--max-w));
+		}
+
+		.br-xl {
+			height: 25rem;
 		}
 	}
 </style>
