0989d01a0733a65ae7f0c10c1bd6c91aee831311
Braden Wiggins <braden@fractal-hq.com>
2024-10-10 08:31:56 -0400

    feat: s5 device

---
+++ src/lib/utils/device.svelte.ts
@@ -0,0 +1,30 @@
+function boilerplate() {
+	const breakpoint =
+		Number(globalThis.document?.documentElement.style.getPropertyValue('--mobile')) || 1000
+
+	let state = $state({
+		width: globalThis.window?.innerWidth || 0,
+		height: globalThis.window?.innerHeight || 0,
+		mobile: globalThis.window?.innerWidth < breakpoint,
+		scrollY: globalThis.window?.scrollY || 0,
+	})
+
+	function onResize() {
+		state.width = globalThis.window?.innerWidth || 0
+		state.height = globalThis.window?.innerHeight || 0
+		state.scrollY = globalThis.window?.scrollY || 0
+		state.mobile = state.width < breakpoint
+	}
+	globalThis.removeEventListener?.('resize', onResize)
+	globalThis.addEventListener?.('resize', onResize)
+
+	function onScroll() {
+		state.scrollY = globalThis.window?.scrollY || 0
+	}
+	globalThis.removeEventListener?.('scroll', onScroll)
+	globalThis.addEventListener?.('scroll', onScroll)
+
+	return state
+}
+
+export const device = boilerplate()
