220247699344508a2dfd6834b501b1969db89297
Braden Wiggins <braden@fractal-hq.com>
2024-10-10 08:31:00 -0400

    feat: new hero

--- src/lib/years/2024/Hero.svelte
+++ src/lib/years/2024/Hero.svelte
@@ -1,32 +1,37 @@
 <script>
 	import TripleChevron from '$lib/graphics/TripleChevron.svelte'
+	import RunesEdition from '$lib/graphics/RunesEdition.svelte'
 </script>
 
 <div class="hero">
-	<h1>SvelteHack 2024</h1>
+	<h1>
+		SvelteHack 2024
+		<div class="runes-edition"><RunesEdition /></div>
+	</h1>
 
-	<div class="br-sm" />
+	<div class="br-sm"></div>
 
-	<p>With not-so-svelte prizes.</p>
+	<p class="a-svelte-hackathon">A Svelte Hackathon</p>
+	<p class="em">(with not-so-svelte prizes)</p>
 
-	<div class="br-sm" />
+	<div class="br-sm"></div>
 
 	<div class="row">
-		<a href="#about">
-			<button class="btn-a">Learn More</button>
-		</a>
-
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<button class="btn-c">
-				Learn more about Svelte&nbsp;<TripleChevron />
-			</button>
+		<a class="button btn-c" href="https://svelte.dev" target="_blank" rel="noreferrer">
+			<span>Learn more about</span>
+			&nbsp;Svelte
+			<span><TripleChevron /></span>
 		</a>
 	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.hero {
+		position: relative;
+
 		max-width: 50%;
+
+		z-index: 2;
 	}
 
 	h1 {
@@ -40,38 +45,105 @@
 		font-size: min(6rem, 15vw);
 	}
 
+	.runes-edition {
+		contain: strict;
+		position: relative;
+		display: inline-flex;
+
+		width: 200px;
+		max-width: 33vw;
+		height: 75px;
+
+		transform: translateY(-10%);
+	}
+
 	p {
+		display: inline-flex;
 		text-align: left;
 
 		margin: 0;
 		color: var(--fg-d);
 
 		font-size: 1.5rem;
+
+		transform: translateY(4px);
+	}
+
+	p.em {
+		font-size: 1.25rem;
+		color: var(--bg-d);
+		font-style: italic;
 	}
 
 	.btn-c {
-		margin-left: 1rem;
-	}
-
-	a {
-		all: unset;
+		position: relative;
 		display: flex;
-	}
-	a:hover {
-		text-decoration: none;
+
+		font-size: var(--font-md);
+		text-decoration: underline;
+		text-underline-offset: 0.25rem;
+		text-decoration-thickness: 0.5px;
+		text-decoration-color: var(--bg-d);
+
+		&::before {
+			content: '';
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			margin: auto 0;
+
+			width: 1px;
+			height: 80%;
+
+			background-color: color-mix(in hsl, var(--brand-a), transparent 60%);
+
+			transform: scaleX(1);
+			transform-origin: center left;
+			transition: transform 0.2s cubic-bezier(0.23, 1, 0.32, 1);
+		}
 	}
 
-	@media screen and (max-width: 1000px) {
-		button {
-			padding: 0.5rem 0.75rem;
+	a:hover,
+	a:active,
+	a:focus {
+		text-decoration-color: transparent;
+
+		&::before {
+			transform: scaleX(0);
 		}
-		.btn-c {
-			margin-left: 0.25rem;
+
+		span {
+			color: var(--fg-a);
+
+			:global(.triple-chevron) {
+				--color: var(--fg-c) !important;
+
+				:global(path) {
+					animation-play-state: running !important;
+				}
+			}
+		}
+	}
+
+	@media (width <= 1000px) {
+		p {
+			display: block;
 		}
 		.hero {
-			max-width: unset;
+			max-width: 100%;
 			width: max-content;
 			padding-left: 1rem;
+		}
+
+		.runes-edition {
+			transform: translateY(0%);
+		}
+
+		.btn-c {
+			font-size: var(--font-sm);
+			padding: 0.5rem 0.75rem;
+			margin-left: 0.25rem;
 		}
 	}
 </style>
