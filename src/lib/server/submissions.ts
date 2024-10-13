63209e8c5f8a7046bfae8083fd2e65fd2940402a
GHOST <ghostdevbusiness@gmail.com>
2024-10-13 20:24:44 +0100

    fix: rules checkbox

--- src/lib/server/submissions.ts
+++ src/lib/server/submissions.ts
@@ -26,7 +26,7 @@ export const submissionSchema = z.object({
 		.trim()
 		.min(1, "Can't be empty")
 		.max(256, 'Max length is 256 chars'),
-	rulesAccepted: z.literal(true, {
+	rulesAccepted: z.literal('on', {
 		message: "Please indicate you've read and agreed to the rules",
 	}),
 })
