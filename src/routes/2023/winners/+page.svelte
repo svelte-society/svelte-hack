dbdaa602bc48acd49e21a3b95dd6c02782ca5b03
Braden Wiggins <braden@fractal-hq.com>
2024-10-15 23:03:26 -0400

    feat: year selector for 2023

--- src/routes/2023/winners/+page.svelte
+++ src/routes/2023/winners/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import Category from './Category.svelte';
-	import type { Winner } from './types';
+	import YearSelector from '$lib/components/YearSelector.svelte'
+	import Category from './Category.svelte'
+	import type { Winner } from './types'
 
 	interface Data {
-		category: string;
-		winners: Winner[];
+		category: string
+		winners: Winner[]
 	}
 
 	const data: Data[] = [
@@ -17,7 +18,7 @@
 					description:
 						'A fully playable port of Wolfenstein 3D 100% rendered by Svelte. No WebGL, or canvas. Just the DOM, CSS, and TypeScript.',
 					github: 'https://github.com/snuffyDev/Wolfensvelte-3D',
-					website: 'https://wolfensvelte-3d.vercel.app/'
+					website: 'https://wolfensvelte-3d.vercel.app/',
 				},
 				{
 					place: 2,
@@ -25,7 +26,7 @@
 					description:
 						'Threltemania is a browser-based fast-paced racing game. Race on campaign tracks or create your own. Share your tracks with friends and let them beat your time.',
 					github: 'https://github.com/grischaerbe/threlter',
-					website: 'https://threltemania.xyz/'
+					website: 'https://threltemania.xyz/',
 				},
 				{
 					place: 3,
@@ -33,23 +34,23 @@
 					description:
 						"Chad-Rpg is an interactive, AI-driven, tabletop style text-based role playing game where chatGPT shapes the story according to player's choices.",
 					github: 'https://github.com/Solemensis/Chad-Rpg',
-					website: 'https://chad-rpg.vercel.app/'
+					website: 'https://chad-rpg.vercel.app/',
 				},
 				{
 					place: 4,
 					title: 'My Queue',
 					description: 'A playlist of text content you want to listen to!',
 					github: 'https://github.com/nick-barth/myqueue',
-					website: 'https://www.myqueue.so/'
+					website: 'https://www.myqueue.so/',
 				},
 				{
 					place: 5,
 					title: 'Jambox',
 					description: 'A collaborative real-time P2P virtual music instrument.',
 					github: 'https://github.com/arslanastral/jambox',
-					website: 'https://jambox.pages.dev/'
-				}
-			]
+					website: 'https://jambox.pages.dev/',
+				},
+			],
 		},
 		{
 			category: '📚 Best Library',
@@ -60,37 +61,37 @@
 					description:
 						'Superforms is a SvelteKit library that helps you with server-side validation and client-side display of forms.',
 					github: 'https://github.com/ciscoheat/sveltekit-superforms',
-					website: 'https://superforms.vercel.app/'
+					website: 'https://superforms.vercel.app/',
 				},
 				{
 					place: 2,
 					title: 'Svelte Zdog',
 					description: 'Svelte components for zdog 3D renders',
 					github: 'https://github.com/wh0am1-dev/svelte-zdog',
-					website: 'https://carlos-aguilar.com/svelte-zdog/'
+					website: 'https://carlos-aguilar.com/svelte-zdog/',
 				},
 				{
 					place: 3,
 					title: 'Radix Svelte',
 					description: 'A port of the popular React library, Radix UI.',
 					github: 'https://github.com/TGlide/radix-svelte',
-					website: 'https://www.radix-svelte.com/'
+					website: 'https://www.radix-svelte.com/',
 				},
 				{
 					place: 4,
 					title: 'Svelte Maplibre',
 					description: 'Svelte support for the Maplibre GL mapping library.',
 					github: 'https://github.com/dimfeld/svelte-maplibre',
-					website: 'https://svelte-maplibre.vercel.app/'
+					website: 'https://svelte-maplibre.vercel.app/',
 				},
 				{
 					place: 5,
 					title: 'Talk2Svelte',
 					description: 'Talk2Svelte is a library providing voice recognition for Svelte.',
 					github: 'https://github.com/ebrehault/talk2svelte',
-					website: 'https://talk2svelte.vercel.app/'
-				}
-			]
+					website: 'https://talk2svelte.vercel.app/',
+				},
+			],
 		},
 		{
 			category: '🔌 Best Ecosystem Integration',
@@ -101,21 +102,21 @@
 					description:
 						'A REPL for SvelteKit projects: quickly spin up a Sveltekit project and share it with the world',
 					github: 'https://github.com/SvelteLab/SvelteLab/',
-					website: 'https://www.sveltelab.dev/'
+					website: 'https://www.sveltelab.dev/',
 				},
 				{
 					place: 2,
 					title: 'Editable Website',
 					description: 'A SvelteKit template for building CMS-free editable websites',
 					github: 'https://github.com/michael/editable-website',
-					website: 'https://editable.website/'
+					website: 'https://editable.website/',
 				},
 				{
 					place: 3,
 					title: 'Sveltris',
 					description: 'Piece together any framework with Svelte like Tetris',
 					github: 'https://github.com/mokshit06/sveltris',
-					website: 'https://sveltris.vercel.app/'
+					website: 'https://sveltris.vercel.app/',
 				},
 				{
 					place: 4,
@@ -123,19 +124,23 @@
 					description:
 						'Svelte inside Phoenix LiveView with seamless end-to-end reactivity',
 					github: 'https://github.com/woutdp/live_svelte',
-					website: 'https://hexdocs.pm/live_svelte'
+					website: 'https://hexdocs.pm/live_svelte',
 				},
 				{
 					place: 5,
 					title: 'Django x Svelte',
 					description: 'Bring the power of Svelte to your Django projects.',
 					github: 'https://github.com/emmyarty/dxsvelte',
-					website: 'https://dxsvelte.com/'
-				}
-			]
-		}
-	];
+					website: 'https://dxsvelte.com/',
+				},
+			],
+		},
+	]
 </script>
+
+<div class="selector">
+	<YearSelector activeYear="2023" />
+</div>
 
 <h1 class="title">Svelte Hack 2023 Winners</h1>
 
@@ -146,6 +151,10 @@
 </div>
 
 <style lang="scss">
+	.selector {
+		margin-left: 2rem;
+	}
+
 	.title {
 		margin: 0px auto;
 		margin-bottom: 48px;
