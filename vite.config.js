f5f400df6f8201eef7448c9b88b38b9970836676
GHOST <ghostdevbusiness@gmail.com>
2024-10-19 17:46:46 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [sveltekit()],
 	ssr: {
-		noExternal: ['three'],
+		noExternal: ['three', 'three-custom-shader-material'],
 	},
 	// silence warnings - https://sass-lang.com/documentation/breaking-changes/legacy-js-api
 	css: { preprocessorOptions: { scss: { api: 'modern' } } },
