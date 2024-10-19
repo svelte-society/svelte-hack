9837d476d7ef99e00db0b2d1e0531605217c4343
GHOST <ghostdevbusiness@gmail.com>
2024-10-19 06:08:01 +0100

    chore: format

--- pocketbase/migrations/1729291122_preferences.go
+++ pocketbase/migrations/1729291122_preferences.go
@@ -11,7 +11,7 @@ import (
 
 func init() {
 	m.Register(func(db dbx.Builder) error {
-		dao := daos.New(db);
+		dao := daos.New(db)
 
 		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
 		if err != nil {
@@ -79,7 +79,7 @@ func init() {
 
 		return dao.SaveCollection(collection)
 	}, func(db dbx.Builder) error {
-		dao := daos.New(db);
+		dao := daos.New(db)
 
 		collection, err := dao.FindCollectionByNameOrId("_pb_users_auth_")
 		if err != nil {
