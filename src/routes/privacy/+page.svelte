e6d8d70530d148b5950ba5cf28ad8d6b1b14f8ea
GHOST <ghostdevbusiness@gmail.com>
2024-10-20 02:46:02 +0100

    chore: add initial privacy policy
    Co-authored-by: Braden Wiggins <braebo@users.noreply.github.com>

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,73 @@
+<div class="br-md"></div>
+
+<section>
+	<div class="card-wrapper">
+		<div class="card">
+			<h1>Privacy Policy</h1>
+
+			<p>
+				We collect limited data in order to manage submissions, which is outlined here. We
+				may make changes to this policy as required, which you can find on this page. If you
+				have any questions please email <code>kev@sveltesociety.dev</code>.
+			</p>
+
+			<h2>Information we collect</h2>
+
+			<ul>
+				<li>Hackathon submission details</li>
+				<li>Your email address</li>
+				<li>Your GitHub username</li>
+				<li>Your Display Name</li>
+				<li>Access logs (such as user agent, request path, etc)</li>
+			</ul>
+
+			<h2>How do we collect your data?</h2>
+
+			<ul>
+				<li>Interacting with our website</li>
+				<li>Submitting your project</li>
+			</ul>
+
+			<h2>How will we use your data?</h2>
+
+			<ul>
+				<li>Processing your submission</li>
+				<li>Communicating with you about your submission</li>
+				<li>
+					Share submissions on our website and social media. Only submission details, and
+					your display name is shared publicly.
+				</li>
+			</ul>
+
+			<h2>Retention</h2>
+
+			<ul>
+				<li>
+					Your data is kept as long as you have an account with us. You can contact us at
+					any time to close your account, and delete all information from our systems
+					except submission details.
+				</li>
+				<li>
+					Submission details are kept so they can be displayed on our winners page, or
+					used on social media.
+				</li>
+				<li>Access logs are kept for a maximum of 3 days</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<div class="br-xl"></div>
+
+<style lang="scss">
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+		line-height: 1.4;
+	}
+
+	p {
+		padding: 1rem;
+	}
+</style>
